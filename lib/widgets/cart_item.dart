1f4464c5a0a5dd9fd8273d6eae5831b34e17c5ff
Harleen Kaur <harleen198.chadha@gmail.com>
2020-07-25 02:48:57 +0530

    creating user products screen

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -36,6 +36,28 @@ class CartItem extends StatelessWidget{
        ),
      ),
      direction: DismissDirection.endToStart,
+     confirmDismiss: (direction){
+      return showDialog(context: context,builder: (ctx) => AlertDialog(        //It returns future when show dialog closes which in end yields true/false
+        title: Text('Are you sure?'),
+        content: Text('Do you want to remove the item from the cart?'),
+        actions: <Widget>[
+         FlatButton(
+           child: Text('No'),
+           onPressed: (){
+             Navigator.of(ctx).pop(false);      //closes the dialog box and gives value to dismissal
+           },
+         ),
+          FlatButton(
+            child: Text('Yes'),
+            onPressed: (){
+              Navigator.of(ctx).pop(true);
+            },
+          ),
+        ],
+      ),
+
+      ) ;
+     },
      onDismissed: (direction){
      Provider.of<Cart>(context, listen: false).removeItem(productId);
      },
