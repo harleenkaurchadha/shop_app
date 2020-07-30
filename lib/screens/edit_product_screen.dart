e81d3bd4525cb364139300c3ece78f951716bdc7
Harleen Kaur <harleen198.chadha@gmail.com>
2020-07-31 00:32:28 +0530

    submitting forms

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../providers/product.dart';
 
 class EditProductScreen extends StatefulWidget{   // because until submission of form it is needed only in local state
   static const routeName = '/edit-product';
@@ -13,6 +14,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
   final _imageFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();                              //global key will help to interact with state behind form widget
+                                                                      //FormState is the state of FORM stateful widget
+  var _editedProduct = Product(
+    id: null,
+    title: '',
+    price: 0,
+    description: '',
+    imageUrl: '',
+  );
 
   @override
   void initState() {
@@ -38,22 +48,47 @@ class _EditProductScreenState extends State<EditProductScreen> {
         });
     }
   }
+  void _saveForm(){
+   _form.currentState.save();
+   print(_editedProduct.title);
+   print(_editedProduct.description);
+   print(_editedProduct.price);
+   print(_editedProduct.imageUrl);
+  }
 
   @override
   Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
        title: Text('Edit Product'),
+       actions: <Widget>[
+         IconButton(
+           icon: Icon(Icons.save),
+           onPressed: _saveForm,
+         ),
+       ],
      ),
      body: Padding(
        padding: EdgeInsets.all(16),
-       child: Form(child: ListView(children: <Widget>[
-        TextFormField(
+       child: Form(
+         key: _form,                                          //now we can use form property to interact with state managed by Form widget
+         child: ListView(
+           children: <Widget>[
+         TextFormField(
           decoration: InputDecoration(labelText: 'Title'),
           textInputAction: TextInputAction.next,                //indicates what bottom right corner of keyboard will show
           onFieldSubmitted: (_){
             FocusScope.of(context).requestFocus(_priceFocusNode);
           },
+           onSaved: (value){
+            _editedProduct = Product(               //since product is final so we can't reassign the value after product is created so we create it again
+              title: value,
+              price: _editedProduct.price,
+              description: _editedProduct.description,
+              imageUrl: _editedProduct.imageUrl,
+              id: null,
+            );
+           },
         ),
          TextFormField(
            decoration: InputDecoration(labelText: 'Price'),
@@ -63,11 +98,30 @@ class _EditProductScreenState extends State<EditProductScreen> {
            onFieldSubmitted: (_){
              FocusScope.of(context).requestFocus(_descriptionFocusNode);
            },
+           onSaved: (value){
+             _editedProduct = Product(               //since product is final so we can't reassign the value after product is created so we create it again
+               title: _editedProduct.title,
+               price: double.parse(value),
+               description: _editedProduct.description,
+               imageUrl: _editedProduct.imageUrl,
+               id: null,
+             );
+           },
          ),
          TextFormField(
            decoration: InputDecoration(labelText: 'Description'),
            maxLines: 3,
            keyboardType: TextInputType.multiline,
+           onSaved: (value) {
+             _editedProduct =
+                 Product( //since product is final so we can't reassign the value after product is created so we create it again
+                   title: _editedProduct.title,
+                   price: _editedProduct.price,
+                   description: value,
+                   imageUrl: _editedProduct.imageUrl,
+                   id: null,
+                 );
+           },
          ),
          Row(
            crossAxisAlignment: CrossAxisAlignment.end,
@@ -96,6 +150,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.done,
               controller: _imageUrlController,                        //since we need url before form is submitted
               focusNode: _imageFocusNode,
+              onFieldSubmitted: (_){
+                _saveForm();
+              },
+              onSaved: (value){
+                _editedProduct = Product(               //since product is final so we can't reassign the value after product is created so we create it again
+                title: _editedProduct.title,
+                price: _editedProduct.price,
+                description: _editedProduct.description,
+                imageUrl: value,
+                id: null,
+                );
+                },
             ),
           ),
          ],
