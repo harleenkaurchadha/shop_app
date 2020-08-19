68cafb4969f3b5c4b5e43297c333ae610f939133
Harleen Kaur <harleen198.chadha@gmail.com>
2020-08-19 23:56:25 +0530

    using future builder for order screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,42 +4,36 @@ import '../widgets/app_drawer.dart';
 import 'package:provider/provider.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget{
+class OrdersScreen extends StatelessWidget{
   static const routeName= '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-  @override
-  void initState(){                               //fetch in initState is good since it runs only once
-//    Future.delayed(Duration.zero).then((_) async{
-      setState(() {
-        _isLoading = true;
-      });
-     Provider.of<Orders>(context,listen: false).fetchAndSetOrders().then((_) { //no need for future delayed with listen set to false
-       setState(() {
-         _isLoading = false;
-       });
-     });
-//    });
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
-    final orderData= Provider.of<Orders>(context);
+    print('building orders');
+//    final orderData= Provider.of<Orders>(context);  it will result in infinite loop since fetchAndSetOrders will notify listeners & it will be called & rebuild
      return Scaffold(
        appBar: AppBar(
          title: Text('Your Orders'),
        ),
        drawer: AppDrawer(),
-       body: _isLoading ? Center(
-         child: CircularProgressIndicator())
-       : ListView.builder(
-           itemCount: orderData.orders.length,
-           itemBuilder: (ctx,i) => OrderItem(orderData.orders[i]),),
-     );
-  }
-}
+       body: FutureBuilder(future: Provider.of<Orders>(context,listen: false).fetchAndSetOrders(),
+             builder: (ctx, dataSnapshot) {
+              if (dataSnapshot.connectionState == ConnectionState.waiting) {          //i.e we are currently loading
+                return Center(
+                    child: CircularProgressIndicator());
+              } else {
+                if(dataSnapshot.error != null) {
+                // Do error handling
+                  return Center(child: Text('An error occured'),);
+                } else {
+                  return Consumer<Orders>(builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx,i) => OrderItem(orderData.orders[i]),
+               ),
+                  );
+                }
+                }
+              },
+              ),
+              );
+            }
+          }
