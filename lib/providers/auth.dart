71e682fe95628400d881954f0949896f964d259f
Harleen Kaur <harleen198.chadha@gmail.com>
2020-08-27 17:05:47 +0530

    user log in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,20 +2,28 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
-class Auth with ChangeNotifier{
+class Auth with ChangeNotifier {
   String _token;
-  DateTime _expiryDate;               // expiry of token
+  DateTime _expiryDate; // expiry of token
   String _userId;
 
-  Future<void> signup(String email, String password) async{
-    const url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDXdqC49gtUCNhMyj4efl0zESbB8lmqhSA";
-    final response = await http.post(url,body: json.encode({
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+    final url = "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDXdqC49gtUCNhMyj4efl0zESbB8lmqhSA";
+    final response = await http.post(url, body: json.encode({
       'email': email,
       'password': password,
-      'returnSecureToken' : true,
+      'returnSecureToken': true,
     },
     ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -107,6 +107,7 @@ class _AuthCardState extends State<AuthCard> {
     });
     if(_authMode == AuthMode.Login){
     // Log user in
+      await Provider.of<Auth>(context, listen: false).login(_authData['email'],_authData['password']);
     } else {
       //Sign user up
       await Provider.of<Auth>(context, listen: false).signup(
