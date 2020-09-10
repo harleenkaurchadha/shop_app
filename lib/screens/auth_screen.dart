0059e038e2b2721ca389d39616b59a27ceb3df33
Harleen Kaur <harleen198.chadha@gmail.com>
2020-09-11 00:06:52 +0530

    animation using animatedContainer

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -103,16 +103,16 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this,                    //vsync is pointer to widget so that animation should start when that widget is seen on screen
-        duration: Duration(
-      milliseconds: 300,
-    ));
-   _heightAnimation = Tween<Size>(begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-       .animate(
-       CurvedAnimation(
-         parent: _controller,
-         curve: Curves.linear,),
-   );
+    // _controller = AnimationController(vsync: this,                    //vsync is pointer to widget so that animation should start when that widget is seen on screen
+    //     duration: Duration(
+    //   milliseconds: 300,
+//    ));
+   // _heightAnimation = Tween<Size>(begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+   //     .animate(
+   //     CurvedAnimation(
+   //       parent: _controller,
+   //       curve: Curves.linear,),
+//   );
  //  _heightAnimation.addListener(() => setState((){}));                    //to run build method to redraw the screen
   }
 
@@ -200,15 +200,16 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
          borderRadius: BorderRadius.circular(10.0),
        ),
        elevation: 8.0,
-       child: AnimatedBuilder(animation: _heightAnimation, builder: (ctx, ch) => Container(               //builder contains what to rerender whenever animation changes
-//         height: _authMode == AuthMode.Signup ? 320: 260,
-         height: _heightAnimation.value.height,
+       child: AnimatedContainer(               //It will detect when the value of container changes & will smoothly animate between the values
+         duration: Duration(milliseconds: 300),
+         curve: Curves.easeIn,
+         height: _authMode == AuthMode.Signup ? 320: 260,
+//         height: _heightAnimation.value.height,
          constraints:
-         BoxConstraints(minHeight: _heightAnimation.value.height),
+         BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320: 260),
          width: deviceSize.width * 0.75,
          padding: EdgeInsets.all(16.0),
-         child: ch),
-         child:  Form(
+         child: Form(
          key: _formKey,
          child: SingleChildScrollView(
            child: Column(
